bbe0a7f1e4e66e6db5550b9844cc27bfa45dba7e
ayayasser01 <ayayasser920@gmail.com>
2025-10-03 13:43:34 +0300

    Add customer routes to app.js

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT =3000;
+
+// enable cors 
+app.use(cors());
+// enable json parser
+app.use(express.json());
+
+// route the customer api 
+const customerRoutes = require('./routes/Customer.route');
+//use the route
+app.use('/api/customers',customerRoutes);
 
 app.get('/',(req,res)=>{
     res.send('Welcome to Customers API !')
