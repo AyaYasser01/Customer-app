ad69c805862e77bda97ee6dc3de74beeca0ebd43
ayayasser01 <ayayasser920@gmail.com>
2025-10-03 11:53:58 +0300

    init

---
+++ backEnd/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT =3000;
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to Customers API !')
+})
+
+app.listen(PORT,(error)=>{
+    if(!error){
+        console.log("Server is successfully listening at port:",PORT);
+    }else{
+        console.error('An error occurred:',error);
+        
+    }
+
+})
